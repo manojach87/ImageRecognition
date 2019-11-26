fe2d26257788189c3e1542aa400ca8c44c3f8581
manojach87 <manoj.ach@gmail.com>
2019-11-26 01:41:05 -0500

    Add files via upload

---
+++ imageRecognition.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 13:00:08 2019
+
+@author: manoj
+"""
+
+import requests
+# If you are using a Jupyter notebook, uncomment the following line.
+#%matplotlib inline
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+
+subscription_key = "327b96aea6b74bd2906557c6db156349"
+#"<Subscription Key>"
+assert subscription_key
+
+vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+
+analyze_url = vision_base_url + "analyze"
+image_path = "DoginCar.jpg"
+
+image_data = open(image_path, "rb").read()
+headers    = {'Ocp-Apim-Subscription-Key': subscription_key,
+              'Content-Type': 'application/octet-stream'}
+params     = {'visualFeatures': 'Categories,Description,Color'}
+response = requests.post(
+    analyze_url, headers=headers, params=params, data=image_data)
+response.raise_for_status()
+
+analysis = response.json()
+print(analysis)
+image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+
+# Display the image and overlay it with the caption.
+image = Image.open(BytesIO(image_data))
+plt.imshow(image)
+plt.axis("off")
+_ = plt.title(image_caption, size="x-large", y=-0.1)
